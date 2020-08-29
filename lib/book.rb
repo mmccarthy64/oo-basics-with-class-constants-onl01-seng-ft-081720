9828a6a6cd33099f9892fe431b6c28a735689f64
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-29 17:52:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
   
-  GENRE = []
+  GENRE = ["Thriller", "Science Fiction", "Romance"]
   
   def initialize(title)
     @title = title
